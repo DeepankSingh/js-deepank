7041c26254e1bc8303e93caa570ef3ad9903eba9
Deepank Singh <74452975+DeepankSingh@users.noreply.github.com>
2024-05-26 06:54:29 +0000

    Symbol object function

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,31 @@
-console.log("Deepank");
+// // Symbol declaration
+
+// const mySym = Symbol("key1");
+
+
+
+// // Object literal declaration
+
+// const Juser = {
+//     name: "Deepank",
+//     roll_no: 345678,
+//     "full name": "Deepnak singh",
+//     name: "Deepank",
+//     roll_no: 345678,
+//     [mySym]: "keykey" //we use [] to refer a symbol. If we don't use [] it will be treated as a normal key or string
+// }
+
+// // How to access an object literal 2 way one is using . operator and other is using []
+// console.log(Juser.name);
+// console.log(Juser["full name"]);
+
+// console.log(Juser);
+
+
+// creating a function in javascript
+
+const greating = function(){
+    console.log("Hello everyone I am deepank");
+}
+
+greating();
